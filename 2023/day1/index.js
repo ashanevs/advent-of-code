e9ba547bd15a16b088ec57db2b5715d24c8fbf59
Shane Vann-Shirley <svann-shirley@socialtables.com>
2023-12-01 10:51:27 -0500

    2023 Day 1 Part 1

--- 2023/day1/index.js
+++ 2023/day1/index.js
@@ -3,29 +3,17 @@ const data = fs.readFileSync("./input.txt", "utf8");
 const dataByLine = data.trim().split("\n");
 
 const result = dataByLine.reduce((acc, curr) => {
-	let firstChar;
-	let lastChar;
-
-	curr.split("").forEach((char, index) => {
-		console.log(!isNaN(parseInt(char, 10)));
-		if (!isNaN(parseInt(char, 10))) {
-			if (!firstChar) {
-				console.log("WHY", firstChar)
-				firstChar = char;
-			} else {
-				lastChar = char;
-			}
+	const numbers = curr.split("").reduce((acc, curr, index) => {
+		if (!isNaN(parseInt(curr, 10))) {
+			acc.push(curr);
 		}
 
-		global.console.log(curr, firstChar, lastChar);
-
-		acc = acc + Number(firstChar + (lastChar || firstChar));
-
-		firstChar = "";
-		lastChar = "";
-
 		return acc;
-	});
+	}, []);
+
+	acc += Number(numbers[0] + numbers[numbers.length - 1]);
+
+	return acc;
 }, 0);
 
 console.log(result);
